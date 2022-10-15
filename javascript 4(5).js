48216036050fcd35a4a0b6f4c349ac489e915786
1sokrat1 <113898340+1sokrat1@users.noreply.github.com>
2022-10-15 14:48:33 +0300

    Add files via upload

---
+++ javascript 4(5).js
@@ -0,0 +1,335 @@
+console.log("1.zadacha")
+var v= "Virno";
+var n= "Nevirno";
+
+let a=1; b=0; c=-3
+
+if(a==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c==0){
+    console.log(v)  
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("2.zadacha")
+
+if(a>0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b>0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c>0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("3.zadacha ")
+
+if(a<0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b<0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c<0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("4.zadacha")
+
+if(a>=0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b>=0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c>=0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("5.zadacha ")
+
+if(a<=0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b<=0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c<=0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("6.zadacha ")
+
+if(a!==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b!==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c!==0){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("7.zadacha ")
+
+let a7="test", b7="TECT", c7=3;
+
+if(a7=="test"){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b7=="test"){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c7=="test"){
+    console.log(v)  
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("8.zadacha ")
+
+let a8 = '1', b8 = 1, c8 = 3;
+if(a8 === '1' && (typeof a8) ===(typeof '1')){
+console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b8 === '1' && (typeof b8) ===(typeof '1')){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c8 === '1' && (typeof c8) ===(typeof '1')){
+	console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("9.zadacha ")
+
+let test=true; test1=false 
+
+if(test==true){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(test1==true){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("10.zadacha ")
+
+if(test!=true){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(test1!=true){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("11.zadacha ")
+
+let a11=5; b11=0; c11=-3; d11=2
+
+if(a11>0 && a11<5){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(b11>0 && b11<5){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(c11>0 && c11<5){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+if(d11>0 && d11<5){
+    console.log(v)
+}
+else{
+    console.log(n)
+}
+
+console.log('\n');
+
+console.log("12.zadacha ")
+
+let a12=5; b12=0; c12=-3; d12=2
+
+if(a12 == 0 || a12 == 2){
+    console.log(a12+7)
+}
+else{
+    console.log(a12/10)
+}
+if(b12 == 0 || b12 == 2){
+    console.log(b12+7)
+}
+else{
+    console.log(b12/10)
+}
+if(c12 == 0 || c12 == 2){
+    console.log(c12+7)
+}
+else{
+    console.log(c12/10)
+}
+if(d12 == 0 || d12 == 2){
+    console.log(d12+7)
+}
+else{
+    console.log(d12/10)
+}
+
+console.log('\n');
+
+console.log("13.zadacha ")
+
+let a13=1; b13=3; c13=0; d13=6; e13=3; f13=5
+
+if(a13<=1 && b13>=3){
+    console.log(a13+b13)
+}
+else{
+    console.log(a13-b13)
+}
+if(c13<=1 && d13>=3){
+    console.log(c13+d13)
+}
+else{
+    console.log(c13-d13)
+}
+if(e13<=1 && f13>=3){
+    console.log(e13+f13)
+}
+else{
+    console.log(e13-f13)
+}
+
+console.log('\n');
+
+console.log("14.zadacha ")
+
+console.log("error404" )
+console.log("student dont understand")
+
+console.log('\n');
+
+console.log("15.zadacha ")
+
+let num=4
+
+switch(num){
+    case 1:
+        console.log("winter")
+    case 2:
+        console.log("spring")
+    case 3:
+        console.log("summer")
+    case 4:
+        console.log("autumn")    
+    }
